5134d68ec8b9fe1f56c3bb0d36cc3e19e314e49f
lin-xi <mcnjqe@163.com>
2016-08-09 15:51:14 +0800

    update

--- socket-client.js
+++ socket-client.js
@@ -13,28 +13,47 @@ function Client(group) {
 Client.prototype.init = function() {
     var me = this;
     me._eventHub = {};
+    me._eventQueue = {};
+
     me.socket = io.connect('ws://10.199.129.14:8999/offline');
     me.socket.on('connect', function(data) {
-        alert('connect true');
         var msg = new Message();
         msg.type = 'join';
         msg.group = me.group;
-        me.socket.send(msg, function(data){
-            alert('join group');
+        me.send(msg, function(data){
             me.state = 'connected';
             me.ready && me.ready();
         });
     });
 
     me.socket.on('message', function(msg) {
-        alert(JSON.stringify(msg));
         var funcs = me._eventHub[msg.type];
         if(funcs && funcs.length > 0){
             funcs.forEach(function(func){
                 func && func(msg);
             });
         }
+
+        var funcs = me._eventQueue[msg.type];
+        while(funcs && funcs.length > 0){
+            var func = funcs.pop();
+            func && func(msg);
+        }
     });
+};
+
+Client.prototype.leave = function(msg, func) {
+    var msg = new Message();
+    msg.type = 'leave';
+    this.send(msg);
+};
+
+Client.prototype.send = function(msg, func) {
+    msg.group = this.group;
+    this.onInnerMessage(msg.type + '_response', function(data){
+        func && func(data);
+    })
+    this.socket.emit(msg.type, msg);
 };
 
 Client.prototype.onMessage = function(evt, func) {
@@ -43,6 +62,14 @@ Client.prototype.onMessage = function(evt, func) {
         me._eventHub[evt] = [];
     }
     me._eventHub[evt].push(func);
+};
+
+Client.prototype.onInnerMessage = function(evt, func) {
+    var me = this;
+    if(!me._eventQueue[evt]){
+        me._eventQueue[evt] = [];
+    }
+    me._eventQueue[evt].push(func);
 };
 
 Client.prototype.ready = function(func) {
@@ -54,13 +81,13 @@ Client.prototype.ready = function(func) {
 };
 
 var group = '{{group}}';
-alert(group);
 var cli = new Client(group);
 cli.ready(function(){
-    alert('ready');
     cli.onMessage('reload', function(data){
-        alert(data.content.url);
-
-        location.href = data.content.url;
+        var result = confirm("检测到有更新，是否重新加载?");
+        if(result){
+            cli.leave();
+            location.href = data.content.url;
+        }
     });
 });
